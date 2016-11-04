d4ddc4611cd6b03db6c10dfb05472584d5f4b63a
lvanhulle <lvanhulle@wisc.edu>
2016-11-04 11:29:57 -0500

    Trying to get folder structure.

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'DogBoneV2',
   packages = ['DogBoneV2'], # this must be the same as the name above
-  version = '2.2.2.3',
+  version = '2.2.2.5',
   description = 'Generating Custom Tool Paths',
   author = 'Luke Van Hulle',
   author_email = 'lvanhulle@wisc.edu',
   url = 'https://github.com/VanHulleOne/DogBoneV2', # use the URL to the github repo
-  download_url = 'https://github.com/VanHulleOne/DogBoneV2/archive/2.2.2.3.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/VanHulleOne/DogBoneV2/archive/2.2.2.5.tar.gz', # I'll explain this in a second
   keywords = ['3D Printing', 'Custom', 'Slicing', 'Tool', 'Path'], # keywords
   classifiers = [],
 )
